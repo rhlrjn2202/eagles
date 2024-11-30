6ffb0e4708441a3793bf563163c64b4535477d8e
rhlrjn2202 <46061702+rhlrjn2202@users.noreply.github.com>
2024-11-30 19:06:43 +0530

    8

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,19 +1,19 @@
 const Logo = () => {
-    return (
-      <div className="flex items-center">
-        <div className="w-14 h-14 -my-2 bg-yellow-400 flex items-center justify-center mr-2 p-2 rounded">
-          <img
-            src="/images/eagle-logo.png"
-            alt="Eagles Cricket Club"
-            className="w-full h-full object-contain"
-          />
-        </div>
-        <span className="text-xl font-bold">
-          <span className="text-white">EAGLES</span>
-          <span className="text-yellow-400">CC</span>
-        </span>
+  return (
+    <div className="flex items-center">
+      <div className="relative w-10 h-10 bg-yellow-400 flex items-center justify-center mr-2 rounded">
+        <img
+          src="/images/eagle-logo.png"
+          alt="Eagles Cricket Club"
+          className="w-8 h-8 object-contain"
+        />
       </div>
-    );
-  };
-  
-  export default Logo;
+      <span className="text-xl font-bold">
+        <span className="text-white">EAGLES</span>
+        <span className="text-yellow-400">CC</span>
+      </span>
+    </div>
+  );
+};
+
+export default Logo;
